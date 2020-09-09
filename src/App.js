470c586c154a5e7e4ea972afe790e8b1d0926350
Gameonn <jindal.ankit89@gmail.com>
2020-09-09 17:23:24 +0530

    changing class to functional component and implemented useState hook

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Person from "./Person/Person";
 import Fruit from "./Fruit/Fruit";
 import "./App.css";
 
-class App extends Component {
-  state = {
+const app = (props) => {
+  const [personState, setPersonState] = useState({
     persons: [
       { name: "Ankit", age: 22, hobby: "mobile games" },
       { name: "David", age: 21, hobby: "coding" },
     ],
+  });
+
+  const [fruitState, setFruitState] = useState({
     fruits: [
       { name: "Mango", color: "yellow", qty: 25 },
       { name: "Orange", color: "orange", qty: 12 },
       { name: "Apple", color: "red", qty: 40 },
       { name: "Guava", color: "green", qty: 15 },
     ],
-  };
+  });
 
-  switchPersonHandler = () => {
-    this.setState({
+  const switchPersonHandler = () => {
+    setPersonState({
       persons: [
-        { name: "Ankit2", age: 28, hobby: "mobile games" },
-        { name: "David2", age: 24, hobby: "coding" },
+        { name: "Ankit2", age: 25, hobby: "FPS games" },
+        { name: "David2", age: 24, hobby: "web development" },
       ],
     });
   };
 
-  changeFruitHandler = () => {
-    this.setState({
+  const changeFruitHandler = () => {
+    setFruitState({
       fruits: [
         { name: "Mango", color: "yellow", qty: 25 },
         { name: "Orange", color: "orange", qty: 12 },
@@ -37,60 +40,52 @@ class App extends Component {
     });
   };
 
-  render() {
-    return (
-      <div className="App">
-        <h1> This is my first react app </h1>
-        <button className="btn-primary" onClick={this.switchPersonHandler}>
-          Switch Person
-        </button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        >
-          {this.state.persons[0].hobby}
-        </Person>
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-        >
-          {this.state.persons[1].hobby}
-        </Person>
+  return (
+    <div className="App">
+      <h1> This is my first react app </h1>
+      <button className="btn-primary" onClick={switchPersonHandler}>
+        Switch Person
+      </button>
+      <Person
+        name={personState.persons[0].name}
+        age={personState.persons[0].age}
+      >
+        {personState.persons[0].hobby}
+      </Person>
+      <Person
+        name={personState.persons[1].name}
+        age={personState.persons[1].age}
+      >
+        {personState.persons[1].hobby}
+      </Person>
 
-        <hr />
-        <h4>We have following fruit stocks:</h4>
-        <button className="btn-warning" onClick={this.changeFruitHandler}>
-          Remove Fruit
-        </button>
-        <Fruit
-          name={this.state.fruits[0].name}
-          qty={this.state.fruits[0].qty}
-          color={this.state.fruits[0].color}
-        />
-        <Fruit
-          name={this.state.fruits[1].name}
-          qty={this.state.fruits[1].qty}
-          color={this.state.fruits[1].color}
-        />
-        <Fruit
-          name={this.state.fruits[2].name}
-          qty={this.state.fruits[2].qty}
-          color={this.state.fruits[2].color}
-        />
-        <Fruit
-          name={this.state.fruits[3].name}
-          qty={this.state.fruits[3].qty}
-          color={this.state.fruits[3].color}
-        />
-      </div>
-    );
+      <hr />
+      <h4>We have following fruit stocks:</h4>
+      <button className="btn-warning" onClick={changeFruitHandler}>
+        Remove Fruit
+      </button>
+      <Fruit
+        name={fruitState.fruits[0].name}
+        qty={fruitState.fruits[0].qty}
+        color={fruitState.fruits[0].color}
+      />
+      <Fruit
+        name={fruitState.fruits[1].name}
+        qty={fruitState.fruits[1].qty}
+        color={fruitState.fruits[1].color}
+      />
+      <Fruit
+        name={fruitState.fruits[2].name}
+        qty={fruitState.fruits[2].qty}
+        color={fruitState.fruits[2].color}
+      />
+      <Fruit
+        name={fruitState.fruits[3].name}
+        qty={fruitState.fruits[3].qty}
+        color={fruitState.fruits[3].color}
+      />
+    </div>
+  );
+};
 
-    // return React.createElement(
-    //   "div",
-    //   { className: App },
-    //   React.createElement("h1", null, "This is my first react app")
-    // );
-  }
-}
-
-export default App;
+export default app;
