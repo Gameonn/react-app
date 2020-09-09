d925209b9115302ba57db16fab2109f2ca91c5c9
Gameonn <jindal.ankit89@gmail.com>
2020-09-09 16:53:50 +0530

    use of state and setState

--- src/App.js
+++ src/App.js
@@ -4,28 +4,84 @@ import Fruit from "./Fruit/Fruit";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Ankit", age: 22, hobby: "mobile games" },
+      { name: "David", age: 21, hobby: "coding" },
+    ],
+    fruits: [
+      { name: "Mango", color: "yellow", qty: 25 },
+      { name: "Orange", color: "orange", qty: 12 },
+      { name: "Apple", color: "red", qty: 40 },
+      { name: "Guava", color: "green", qty: 15 },
+    ],
+  };
+
+  switchPersonHandler = () => {
+    this.setState({
+      persons: [
+        { name: "Ankit2", age: 28, hobby: "mobile games" },
+        { name: "David2", age: 24, hobby: "coding" },
+      ],
+    });
+  };
+
+  changeFruitHandler = () => {
+    this.setState({
+      fruits: [
+        { name: "Mango", color: "yellow", qty: 25 },
+        { name: "Orange", color: "orange", qty: 12 },
+        { name: "grapes", color: "brown", qty: 40 },
+        { name: "Guava", color: "green", qty: 15 },
+      ],
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <h1> This is my first react app </h1>
-        <Person name="Ankit" age="25">
-          I love playing FPS games
+        <button className="btn-primary" onClick={this.switchPersonHandler}>
+          Switch Person
+        </button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        >
+          {this.state.persons[0].hobby}
         </Person>
-        <Person name="David" age="21" />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
+          {this.state.persons[1].hobby}
+        </Person>
+
         <hr />
         <h4>We have following fruit stocks:</h4>
-        <Fruit name="Mango" qty="25">
-          color as Yellow
-        </Fruit>
-        <Fruit name="Banana" qty="36">
-          color as Yellow
-        </Fruit>
-        <Fruit name="Apple" qty="50">
-          color as Red
-        </Fruit>
-        <Fruit name="Orange" qty="12">
-          color as Orange
-        </Fruit>
+        <button className="btn-warning" onClick={this.changeFruitHandler}>
+          Remove Fruit
+        </button>
+        <Fruit
+          name={this.state.fruits[0].name}
+          qty={this.state.fruits[0].qty}
+          color={this.state.fruits[0].color}
+        />
+        <Fruit
+          name={this.state.fruits[1].name}
+          qty={this.state.fruits[1].qty}
+          color={this.state.fruits[1].color}
+        />
+        <Fruit
+          name={this.state.fruits[2].name}
+          qty={this.state.fruits[2].qty}
+          color={this.state.fruits[2].color}
+        />
+        <Fruit
+          name={this.state.fruits[3].name}
+          qty={this.state.fruits[3].qty}
+          color={this.state.fruits[3].color}
+        />
       </div>
     );
 
--- src/Fruit/Fruit.js
+++ src/Fruit/Fruit.js
@@ -1,10 +1,12 @@
 import React from "react";
+import "./Fruit.css";
 
 const fruit = (props) => {
   return (
     <div className="Fruit">
-      <strong> {props.name}</strong> is available in <i>{props.qty}</i> quantity
-      in <s>{props.children}</s>
+      <div className={props.color}>
+        <strong> {props.name}</strong> is available in <i>{props.qty}</i>
+      </div>
     </div>
   );
 };
